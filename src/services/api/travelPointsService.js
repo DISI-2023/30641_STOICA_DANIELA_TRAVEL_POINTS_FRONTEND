1336e5592af77e11cee8e2cd41ad42eb95d458f6
MatiocBogdanDarius <72139770+MatiocBogdanDarius@users.noreply.github.com>
2023-04-20 17:36:20 +0300

    remove redundant comments

--- src/services/api/travelPointsService.js
+++ src/services/api/travelPointsService.js
@@ -1,11 +1,9 @@
 import {travelPointsAxios} from "./axios";
 
-//USER
 const login = async (credentials) => {
     return await travelPointsAxios.post("user/authenticate", JSON.stringify(credentials));
 }
 
-//LANDMARK
 const deleteLandmark = async (id) => {
     return await travelPointsAxios.delete(`landmark/${id}`)
 }
